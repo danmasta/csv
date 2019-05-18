d08637fe76c57a43498e5e18bfe6aade71c3655b
Daniel Smith <dannmasta@gmail.com>
2019-05-17 19:34:12 -0700

    add unit test for multibyte characters

---
+++ tests/multibyte-chars.js
@@ -0,0 +1,22 @@
+const _ = require('lodash');
+
+describe('multibyte-chars', () => {
+
+    it('should accurately multibyte characters across chunk boundaries', done => {
+
+        let data = Buffer.from(`col0,col1,col2\né,£,€`);
+        let stream = util.readstream(util.toCharArray(data), { encoding: null });
+        let res = [];
+
+        return stream.pipe(csv.stream())
+            .on('data', chunk => {
+                res.push(chunk);
+            })
+            .once('end', () => {
+                expect(_.isEqual(res, [ { col0: 'é', col1: '£', col2: '€' } ]));
+                done();
+            });
+
+    });
+
+});
